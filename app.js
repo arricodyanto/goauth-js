8c5ce7e5b43663396eceaabfdd112a6758bd1535
arricodyanto <arricohandyanto@gmail.com>
2021-11-01 21:50:13 +0700

    update conn to mongo atlas

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 require('./auth')
 require('./utils/db')
 
--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 const userModel = require('./users');
-mongoose.connect(process.env.MONGODB_URI, {
+const uri = process.env.MONGODB_URI
+mongoose.connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
